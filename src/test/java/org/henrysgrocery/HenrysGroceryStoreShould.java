0e950a8554771b9e57968b73c7c91fa5ff42b7e4
Andy Muir <andy.muir@aimyourtechnology.com>
2020-04-01 14:11:39 +0100

    Create AcceptanceTest - working

---
+++ src/test/java/org/henrysgrocery/HenrysGroceryStoreShould.java
@@ -0,0 +1,12 @@
+package org.henrysgrocery;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class HenrysGroceryStoreShould {
+    @Test
+    void nothing() {
+        assertTrue(true);
+    }
+}
