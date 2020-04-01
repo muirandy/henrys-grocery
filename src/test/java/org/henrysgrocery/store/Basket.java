86ca53fae746d6848bd668f927fe779a848bffde
Andy Muir <andy.muir@aimyourtechnology.com>
2020-04-01 15:48:23 +0100

    Refactor: Basket breaks SRP. Lets get the Pricing outside.

--- src/test/java/org/henrysgrocery/store/Basket.java
+++ src/test/java/org/henrysgrocery/store/Basket.java
@@ -5,15 +5,16 @@ import java.time.LocalDate;
 import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Basket {
     private List<Item> items = new ArrayList<>();
 
-    public static Basket create() {
-        return new Basket();
+    private Basket() {
     }
 
-    private Basket() {
+    public static Basket create() {
+        return new Basket();
     }
 
     public Basket add(int quantity, Item item) {
@@ -23,27 +24,35 @@ public class Basket {
     }
 
     public BigDecimal priceUp(LocalDate purchaseDate) {
-        return items.stream()
+        return new BasketPricer(purchaseDate).priceUp(items.stream());
+    }
+
+    private class BasketPricer {
+
+        private LocalDate purchaseDate;
+
+        public BasketPricer(LocalDate purchaseDate) {
+            this.purchaseDate = purchaseDate;
+        }
+
+        public BigDecimal priceUp(Stream<Item> items) {
+            return items
                     .map(i -> i.price)
                     .map(p -> applyDiscount(purchaseDate, p))
                     .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
+        }
 
-    private BigDecimal applyDiscount(LocalDate purchaseDate, BigDecimal p) {
-        if (discountApplies(purchaseDate))
-            return p.multiply(BigDecimal.valueOf(0.9));
-        return p;
-    }
+        private BigDecimal applyDiscount(LocalDate purchaseDate, BigDecimal p) {
+            if (discountApplies(purchaseDate))
+                return p.multiply(BigDecimal.valueOf(0.9));
+            return p;
+        }
 
-    private boolean discountApplies(LocalDate purchaseDate) {
-        LocalDate today = LocalDate.now();
-        LocalDate dayAfterEndOfNextMonth = today.plusMonths(2).with(TemporalAdjusters.firstDayOfMonth());
-        return purchaseDate.isAfter(today.plusDays(2))
-                && purchaseDate.isBefore(dayAfterEndOfNextMonth);
-    }
-
-    public Basket empty() {
-        items.clear();
-        return this;
+        private boolean discountApplies(LocalDate purchaseDate) {
+            LocalDate today = LocalDate.now();
+            LocalDate dayAfterEndOfNextMonth = today.plusMonths(2).with(TemporalAdjusters.firstDayOfMonth());
+            return purchaseDate.isAfter(today.plusDays(2))
+                    && purchaseDate.isBefore(dayAfterEndOfNextMonth);
+        }
     }
 }
