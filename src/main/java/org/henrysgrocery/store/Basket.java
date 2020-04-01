3de319016efe81259565740a561e08687a5bddcf
Andy Muir <andy.muir@aimyourtechnology.com>
2020-04-01 16:16:43 +0100

    Refactor: Extract BasketPricer to new class

--- src/main/java/org/henrysgrocery/store/Basket.java
+++ src/main/java/org/henrysgrocery/store/Basket.java
@@ -2,19 +2,17 @@ package org.henrysgrocery.store;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Stream;
 
 public class Basket {
     private List<Item> items = new ArrayList<>();
 
-    private Basket() {
-    }
-
     public static Basket create() {
         return new Basket();
+    }
+
+    private Basket() {
     }
 
     public Basket add(int quantity, Item item) {
@@ -27,32 +25,4 @@ public class Basket {
         return new BasketPricer(purchaseDate).priceUp(items.stream());
     }
 
-    private class BasketPricer {
-
-        private LocalDate purchaseDate;
-
-        public BasketPricer(LocalDate purchaseDate) {
-            this.purchaseDate = purchaseDate;
-        }
-
-        public BigDecimal priceUp(Stream<Item> items) {
-            return items
-                    .map(i -> i.price)
-                    .map(p -> applyDiscount(purchaseDate, p))
-                    .reduce(BigDecimal.ZERO, BigDecimal::add);
-        }
-
-        private BigDecimal applyDiscount(LocalDate purchaseDate, BigDecimal p) {
-            if (discountApplies(purchaseDate))
-                return p.multiply(BigDecimal.valueOf(0.9));
-            return p;
-        }
-
-        private boolean discountApplies(LocalDate purchaseDate) {
-            LocalDate today = LocalDate.now();
-            LocalDate dayAfterEndOfNextMonth = today.plusMonths(2).with(TemporalAdjusters.firstDayOfMonth());
-            return purchaseDate.isAfter(today.plusDays(2))
-                    && purchaseDate.isBefore(dayAfterEndOfNextMonth);
-        }
-    }
 }
---
+++ src/main/java/org/henrysgrocery/store/BasketPricer.java
@@ -0,0 +1,35 @@
+package org.henrysgrocery.store;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.temporal.TemporalAdjusters;
+import java.util.stream.Stream;
+
+class BasketPricer {
+
+    private LocalDate purchaseDate;
+
+    public BasketPricer(LocalDate purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public BigDecimal priceUp(Stream<Item> items) {
+        return items
+                .map(i -> i.price)
+                .map(p -> applyDiscount(purchaseDate, p))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal applyDiscount(LocalDate purchaseDate, BigDecimal p) {
+        if (discountApplies(purchaseDate))
+            return p.multiply(BigDecimal.valueOf(0.9));
+        return p;
+    }
+
+    private boolean discountApplies(LocalDate purchaseDate) {
+        LocalDate today = LocalDate.now();
+        LocalDate dayAfterEndOfNextMonth = today.plusMonths(2).with(TemporalAdjusters.firstDayOfMonth());
+        return purchaseDate.isAfter(today.plusDays(2))
+                && purchaseDate.isBefore(dayAfterEndOfNextMonth);
+    }
+}
