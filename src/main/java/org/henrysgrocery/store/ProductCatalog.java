d776ca3927a7a8ac8285ef207f59f06911aa1a7d
Andy Muir <andy.muir@aimyourtechnology.com>
2020-04-02 10:11:16 +0100

    Refactor - improve naming

--- src/main/java/org/henrysgrocery/store/ProductCatalog.java
+++ src/main/java/org/henrysgrocery/store/ProductCatalog.java
@@ -11,7 +11,7 @@ public class ProductCatalog {
     private ProductCatalog() {
     }
 
-    private Map<Item, BigDecimal> prices = Map.of(
+    private Map<Item, BigDecimal> catalog = Map.of(
             new Item(Unit.TIN, "soup"), BigDecimal.valueOf(0.65),
             new Item(Unit.LOAF, "bread"), BigDecimal.valueOf(0.80),
             new Item(Unit.BOTTLE, "milk"), BigDecimal.valueOf(1.30),
@@ -19,6 +19,6 @@ public class ProductCatalog {
     );
 
     public BigDecimal getPrice(Item item) {
-        return prices.get(item);
+        return catalog.get(item);
     }
 }
