e35192afdabaa459db6b43edf7e3104aee69f53d
Andy Muir <andy.muir@aimyourtechnology.com>
2020-04-02 14:48:40 +0100

    Quit on receiving any input

--- src/main/java/org/henrysgrocery/store/CommandLineInterface.java
+++ src/main/java/org/henrysgrocery/store/CommandLineInterface.java
@@ -2,13 +2,28 @@ package org.henrysgrocery.store;
 
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 public class CommandLineInterface {
+    private InputStream in;
+    private PrintStream out;
+
     public void run(InputStream in, PrintStream out) {
+        this.in = in;
+        this.out = out;
+
+        Scanner scanner = new Scanner(in);
         displayHeading(out);
+        if (scanner.hasNext())
+            displayExitMessage();
     }
 
     private void displayHeading(PrintStream out) {
         out.println("Henrys Store");
     }
+
+    private void displayExitMessage() {
+        out.println("Exit");
+    }
+
 }
--- src/test/java/org/henrysgrocery/store/CommandLineInterfaceTest.java
+++ src/test/java/org/henrysgrocery/store/CommandLineInterfaceTest.java
@@ -7,23 +7,40 @@ import java.io.*;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 class CommandLineInterfaceTest {
+
+    private CommandLineInterface commandLineInterface = new CommandLineInterface();
+    private ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    private PrintStream printStream = new PrintStream(outputStream);
+    private InputStream inputStream;
+
+
     @Test
     void displaysHeader() {
-        CommandLineInterface commandLineInterface = new CommandLineInterface();
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        PrintStream printStream = new PrintStream(outputStream);
-        InputStream inputStream = createInputStream("");
+        inputStream = createInputStream("");
 
         commandLineInterface.run(inputStream, printStream);
 
-        assertConsoleOutput(outputStream);
+        assertLastConsoleOutput("Henrys Store");
+    }
+
+    @Test
+    void quits() {
+        inputStream = createInputStream("quit");
+
+        commandLineInterface.run(inputStream, printStream);
+
+        assertLastConsoleOutput("Exit");
     }
 
     private ByteArrayInputStream createInputStream(String s) {
         return new ByteArrayInputStream(s.getBytes());
     }
 
-    private void assertConsoleOutput(ByteArrayOutputStream outputStream) {
-        assertThat(outputStream.toString()).isEqualToIgnoringNewLines("Henrys Store");
+    private void assertLastConsoleOutput(String expected) {
+        String actual = outputStream.toString();
+        String[] lines = actual.split(System.lineSeparator());
+        String lastLine = lines[lines.length - 1];
+
+        assertThat(lastLine).isEqualToIgnoringNewLines(expected);
     }
 }
